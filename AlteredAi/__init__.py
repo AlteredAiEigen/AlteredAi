433b8b13bcb78cbfe1e31da4752aa0a2ca55e6a4
chanpreet <channusingh97@gmail.com>
2023-03-25 05:34:47 +0530

    restructured

--- AlteredAi/__init__.py
+++ AlteredAi/__init__.py
@@ -1,13 +1,13 @@
 from . import core
-from core import *
+
 
 from . import ivy
-from ivy import *
+
 
 from . import torch
-from torch import *
+
 
 from . import tensorflow
-from tensorflow import *
+
 
 
